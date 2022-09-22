3a50896b5d50584ed822d69689bed69a462dea28
RennanD <rplaykl@gmail.com>
2022-09-22 18:10:53 -0300

    Component Library - Customizando o tema default

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { StatusBar, View } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import { THEME } from './src/theme';
 
 import { Loading } from '@components/Loading';
 
@@ -9,13 +10,13 @@ export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar 
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      <Loading />
+      {fontsLoaded ? <View /> : <Loading />}
     </NativeBaseProvider>
   );
 }
--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,8 +2,8 @@ import { Center, Spinner } from 'native-base';
 
 export function Loading() {
   return (
-    <Center flex={1}>
-      <Spinner />
+    <Center flex={1} bg="gray.700">
+      <Spinner color="green.500" />
     </Center>
   );
 }
