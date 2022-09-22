039db9cb3c3ea8da8cbb49a80974e92f62510480
RennanD <rplaykl@gmail.com>
2022-09-22 17:14:54 -0300

    Estrutura da aplicação - Personalizando a StatusBar

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
-import { Text, View } from 'react-native';
+import { StatusBar, Text, View } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#202024' }}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Text>Hello World</Text> : <View />}
     </View>
   );
