e0fe4f993206e35a553b5c2ef9eab8fe2509df43
RennanD <rplaykl@gmail.com>
2022-09-26 16:16:56 -0300

    Rotas Públicas - Criando o contexto de navegação

--- App.tsx
+++ App.tsx
@@ -2,10 +2,11 @@ import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import { Routes } from '@routes/index';
+
 import { THEME } from './src/theme';
 
 import { Loading } from '@components/Loading';
-import { SignUp } from '@screens/SignUp';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -17,7 +18,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignUp /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { NavigationContainer } from "@react-navigation/native";
+
+import { AuthRoutes } from "./auth.routes";
+
+export function Routes() {
+  return (
+    <NavigationContainer>
+      <AuthRoutes />
+    </NavigationContainer>
+  );
+}
