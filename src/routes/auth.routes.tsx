b061fdb67f5dea55b013af56a41b5299c3fc0a7f
RennanD <rplaykl@gmail.com>
2022-09-26 16:06:40 -0300

    Rotas Públicas - Tipando as Rotas

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,8 +1,16 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator, NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { SignIn } from '@screens/SignIn';
 import { SignUp } from '@screens/SignUp';
 
-const { Navigator, Screen } = createNativeStackNavigator();
+
+type AuthRoutes = {
+  signIn: undefined;
+  signUp: undefined;
+}
+
+export type AuthNavigatorRoutesProps = NativeStackNavigationProp<AuthRoutes>;
+
+const { Navigator, Screen } = createNativeStackNavigator<AuthRoutes>();
 
 export function AuthRoutes() {
   return (
@@ -11,7 +19,7 @@ export function AuthRoutes() {
         name="signIn"
         component={SignIn}
       />
-      
+
       <Screen 
         name="signUp"
         component={SignUp}
