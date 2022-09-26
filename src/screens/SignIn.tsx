7e66b4ed6506c3decc728bc239cbd80e4a7166f2
RennanD <rplaykl@gmail.com>
2022-09-26 16:34:52 -0300

    Rotas Públicas - Definindo a imagem padrão

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -22,6 +22,7 @@ export function SignIn() {
         <VStack flex={1} px={10} pb={16}>
         <Image 
           source={BackgroundImg}
+          defaultSource={BackgroundImg}
           alt="Pessoas treinando"
           resizeMode="contain"
           position="absolute"
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -20,6 +20,7 @@ export function SignUp() {
         <VStack flex={1} px={10} pb={16}>
         <Image 
           source={BackgroundImg}
+          defaultSource={BackgroundImg}
           alt="Pessoas treinando"
           resizeMode="contain"
           position="absolute"
