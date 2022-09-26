2fb7b444874e7710474f21d48d71c73e5b47307d
RennanD <rplaykl@gmail.com>
2022-09-26 15:50:28 -0300

    Component Library - Criando a SignUp

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 import { THEME } from './src/theme';
 
 import { Loading } from '@components/Loading';
-import { SignIn } from '@screens/SignIn';
+import { SignUp } from '@screens/SignUp';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -17,7 +17,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <SignUp /> : <Loading />}
     </NativeBaseProvider>
   );
 }
---
+++ src/screens/SignUp.tsx
@@ -0,0 +1,59 @@
+import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+
+import LogoSvg from '@assets/logo.svg';
+import BackgroundImg from '@assets/background.png';
+
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
+
+export function SignUp() {
+  return (
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
+        <VStack flex={1} bg="gray.700" px={10} pb={16}>
+        <Image 
+          source={BackgroundImg}
+          alt="Pessoas treinando"
+          resizeMode="contain"
+          position="absolute"
+        />
+
+        <Center my={24}>
+          <LogoSvg />
+
+          <Text color="gray.100" fontSize="sm">
+            Treine sua mente e o seu corpo.
+          </Text>
+        </Center>
+
+        <Center>
+          <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+            Crie sua conta
+          </Heading>
+
+          <Input 
+            placeholder="Nome"
+          />
+
+          <Input 
+            placeholder="E-mail" 
+            keyboardType="email-address"
+            autoCapitalize="none"
+
+          />
+          <Input 
+            placeholder="Senha" 
+            secureTextEntry
+          />
+
+          <Button title="Criar e acessar" />
+        </Center>
+        
+        <Button 
+          title="Voltar para o login" 
+          variant="outline" 
+          mt={24}
+        />
+      </VStack>
+    </ScrollView>
+  );
+}
