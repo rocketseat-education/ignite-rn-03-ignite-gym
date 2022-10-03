847dd634e709495b31b4f19a0e860955c0130067
RennanD <rplaykl@gmail.com>
2022-10-03 17:07:54 -0300

    Rotas Privadas - Estilizando menu ativo e inativos

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -23,14 +23,16 @@ const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
 export function AppRoutes() {
 
-  const { sizes } = useTheme();
+  const { sizes, colors } = useTheme();
 
   const iconSize = sizes[6];
 
   return (
     <Navigator screenOptions={{ 
       headerShown: false,
-      tabBarShowLabel: false
+      tabBarShowLabel: false,
+      tabBarActiveTintColor: colors.green[500],
+      tabBarInactiveTintColor: colors.gray[200]
     }}>
       <Screen 
         name='home'
