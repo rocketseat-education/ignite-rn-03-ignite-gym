c88df43a2b3192b35c94e7d1f583e41be208880b
RennanD <rplaykl@gmail.com>
2022-10-03 16:25:40 -0300

    Rotas Privadas - Utilizando o Bottom Tab

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { useTheme, Box } from 'native-base';
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 
 import { AuthRoutes } from "./auth.routes";
+import { AppRoutes } from "./app.routes";
 
 export function Routes() {
   const { colors } = useTheme();
@@ -12,7 +13,7 @@ export function Routes() {
   return (
     <Box flex={1} bg="gray.700">
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        <AppRoutes />
       </NavigationContainer>
     </Box>
   );
