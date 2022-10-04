613b45705e29b6a488aed9a2ca0cba5087120128
RennanD <rplaykl@gmail.com>
2022-10-04 17:21:35 -0300

    Finalizando as Interfaces - Foto do usuário

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,11 +1,22 @@
-import { VStack } from 'native-base';
+import { Center, ScrollView, VStack } from 'native-base';
 
 import { ScreenHeader } from '@components/ScreenHeader';
+import { UserPhoto } from '@components/UserPhoto';
 
 export function Profile() {
   return (
     <VStack flex={1}>
       <ScreenHeader title='Perfil' />
+
+      <ScrollView>
+        <Center mt={6} px={10}>
+          <UserPhoto 
+            source={{ uri: 'https://github.com/rodrigorgtic.png' }}
+            alt="Foto do usuário"
+            size={33}
+          />
+        </Center>
+      </ScrollView>
     </VStack>
   );
 }
