cd0e222b6ed97027fc08596f2d905e13bed61946
RennanD <rplaykl@gmail.com>
2022-10-04 14:05:03 -0300

    Finalizando as Interfaces - Componente UserPhoto

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,8 +1,17 @@
 import { Heading, HStack, Text, VStack } from 'native-base';
 
+import { UserPhoto } from './UserPhoto';
+
 export function HomeHeader() {
   return (
     <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <UserPhoto 
+        source={{ uri: 'https://github.com/rodrigorgtic.png' }}
+        size={16}
+        alt="Imagem do usuário"
+        mr={4}
+      />
+      
       <VStack>
         <Text color="gray.100" fontSize="md">
           Olá,
