a005d3b996159e30db34ad9ec324593fd1bed0c7
RennanD <rplaykl@gmail.com>
2022-10-04 14:08:49 -0300

    Finalizando as Interfaces - Botão Logout

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,4 +1,6 @@
-import { Heading, HStack, Text, VStack } from 'native-base';
+import { TouchableOpacity } from 'react-native';
+import { Heading, HStack, Text, VStack, Icon } from 'native-base';
+import { MaterialIcons } from '@expo/vector-icons';
 
 import { UserPhoto } from './UserPhoto';
 
@@ -21,6 +23,16 @@ export function HomeHeader() {
           Rodrigo
         </Heading>
       </VStack>
+
+
+      <TouchableOpacity>
+        <Icon 
+          as={MaterialIcons}
+          name="logout"
+          color="gray.200"
+          size={7}
+        />
+      </TouchableOpacity>
     </HStack>
   );
 }
