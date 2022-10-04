d18301d705b2887ab2e04efbe41d6e129c048cf3
RennanD <rplaykl@gmail.com>
2022-10-04 16:54:34 -0300

    Finalizando as Interfaces - Componente HistoryCard

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,10 +1,16 @@
-import { ScreenHeader } from '@components/ScreenHeader';
 import { VStack } from 'native-base';
+
+import { HistoryCard } from '@components/HistoryCard';
+import { ScreenHeader } from '@components/ScreenHeader';
 
 export function History() {
   return (
     <VStack flex={1}>
       <ScreenHeader title='Histórico' />
+
+      <HistoryCard />
+      <HistoryCard />
+      <HistoryCard />
     </VStack>
   );
 }
