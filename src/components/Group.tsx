48a8d3659cfedf86656bf909c8cb00d184193486
RennanD <rplaykl@gmail.com>
2022-10-04 14:35:45 -0300

    Finalizando as Interfaces - Utilizando o Pressable

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -1,18 +1,30 @@
-import { Text } from 'native-base';
+import { Text, Pressable, IPressableProps } from 'native-base';
 
-type Props = {
+type Props = IPressableProps & {
   name: string;
 }
 
-export function Group({ name }: Props) {
+export function Group({ name, ...rest }: Props) {
   return (
-    <Text
-      color="gray.200"
-      textTransform="uppercase"
-      fontSize="xs"
-      fontWeight="bold"
+    <Pressable
+      mr={3}
+      w={24}
+      h={10}
+      bg="gray.600"
+      rounded="md"
+      justifyContent="center"
+      alignItems="center"
+      overflow="hidden"
+      {...rest}
     >
-      {name}
-    </Text>
+      <Text
+        color="gray.200"
+        textTransform="uppercase"
+        fontSize="xs"
+        fontWeight="bold"
+      >
+        {name}
+      </Text>
+    </Pressable>
   );
 }
