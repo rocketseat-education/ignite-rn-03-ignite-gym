2d70a112732e9f9245b0e46b26275f43d65856e7
RennanD <rplaykl@gmail.com>
2022-10-04 15:46:09 -0300

    Finalizando as Interfaces - Recap da Home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -21,7 +21,7 @@ export function Home() {
         renderItem={({ item }) => (
           <Group 
             name={item}
-            isActive={groupSelected === item}
+            isActive={groupSelected.toLocaleUpperCase() === item.toLocaleUpperCase()}
             onPress={() => setGroupSelected(item)}
           />
         )}
