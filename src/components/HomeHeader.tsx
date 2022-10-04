c54f5e27c8e96da0bc59f61ed11a41472721c3c3
RennanD <rplaykl@gmail.com>
2022-10-04 13:53:03 -0300

    Finalizando as Interfaces - Posicionando na vertical e horizontal

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,17 @@
+import { Heading, HStack, Text, VStack } from 'native-base';
+
+export function HomeHeader() {
+  return (
+    <HStack>
+      <VStack>
+        <Text color="gray.100">
+          Olá,
+        </Text>
+
+        <Heading color="gray.100">
+          Rodrigo
+        </Heading>
+      </VStack>
+    </HStack>
+  );
+}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,10 @@
-import { Center, Text } from 'native-base';
+import { HomeHeader } from '@components/HomeHeader';
+import { Text, VStack } from 'native-base';
 
 export function Home() {
   return (
-    <Center flex={1}>
-      <Text color="white">Home</Text>
-    </Center>
+    <VStack flex={1}>
+      <HomeHeader />
+    </VStack>
   );
 }
