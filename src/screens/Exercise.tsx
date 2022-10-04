f2f153f90465f4b3b0b2ab8317732dcb9913e996
RennanD <rplaykl@gmail.com>
2022-10-04 19:01:37 -0300

    Finalizando as Interfaces - Finalizando o Header do Exercício

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,8 +1,11 @@
 import { TouchableOpacity } from 'react-native';
-import { Icon, VStack } from 'native-base';
+import { Heading, HStack, Icon, Text, VStack } from 'native-base';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+
 import { AppNavigatorRoutesProps } from '@routes/app.routes';
+
+import BodySvg from '@assets/body.svg';
 
 export function Exercise() {
 
@@ -23,6 +26,20 @@ export function Exercise() {
             size={6}
           />
         </TouchableOpacity>
+
+        <HStack justifyContent="space-between" mt={4} mb={8} alignItems="center">
+          <Heading color="gray.100" fontSize="lg"  flexShrink={1}>
+            Puxada frontal
+          </Heading>
+
+          <HStack alignItems="center">
+            <BodySvg />
+
+            <Text color="gray.200" ml={1} textTransform="capitalize">
+              Costas
+            </Text>
+          </HStack>
+        </HStack>
       </VStack>
     </VStack>
   );
