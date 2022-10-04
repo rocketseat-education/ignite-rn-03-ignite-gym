984c5c3832bb9c3935bbcb3882af5a19d56873e7
RennanD <rplaykl@gmail.com>
2022-10-04 17:04:57 -0300

    Finalizando as Interfaces - Utilizando SectionList

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,16 +1,39 @@
-import { VStack } from 'native-base';
+import { useState } from 'react';
+import { Heading, VStack, SectionList } from 'native-base';
 
 import { HistoryCard } from '@components/HistoryCard';
 import { ScreenHeader } from '@components/ScreenHeader';
 
 export function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: '26.08.22',
+      data: ["Puxada frontal", "Remada unilateral"]
+    },
+    {
+      title: '27.08.22',
+      data: ["Puxada frontal"]
+    }
+  ]);
+
   return (
     <VStack flex={1}>
       <ScreenHeader title='Histórico' />
 
-      <HistoryCard />
-      <HistoryCard />
-      <HistoryCard />
+      <SectionList 
+        sections={exercises}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <HistoryCard />
+        )}
+        renderSectionHeader={({ section }) => (
+          <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        px={8}
+      />
+
     </VStack>
   );
 }
