fb0ca880b48fe59f4f07580be3e5c016a33e6d02
RennanD <rplaykl@gmail.com>
2022-10-04 18:07:25 -0300

    Finalizando as Interfaces - Inputs de senha

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 import { TouchableOpacity } from 'react-native';
-import { Center, ScrollView, VStack, Skeleton, Text } from 'native-base';
+import { Center, ScrollView, VStack, Skeleton, Text, Heading } from 'native-base';
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
 import { Input } from '@components/Input';
+import { Button } from '@components/Button';
 
 
 const PHOTO_SIZE = 33;
@@ -53,6 +54,32 @@ export function Profile() {
             isDisabled
           />
         </Center>
+
+        <VStack px={10} mt={12} mb={9}>
+          <Heading color="gray.200" fontSize="md" mb={2}>
+            Alterar senha
+          </Heading>
+
+          <Input 
+            bg="gray.600"
+            placeholder="Senha antiga"
+            secureTextEntry
+          />
+
+          <Input 
+            bg="gray.600"
+            placeholder="Nova senha"
+            secureTextEntry
+          />
+
+          <Input 
+            bg="gray.600"
+            placeholder="Confirme a nova senha"
+            secureTextEntry
+          />
+
+          <Button title="Atualizar" mt={4} />
+        </VStack>
       </ScrollView>
     </VStack>
   );
