e207e68092802d86c90a594d57982f92977214bc
RennanD <rplaykl@gmail.com>
2022-10-04 13:55:06 -0300

    Finalizando as Interfaces - Estilizando o HomeHeader

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -2,13 +2,13 @@ import { Heading, HStack, Text, VStack } from 'native-base';
 
 export function HomeHeader() {
   return (
-    <HStack>
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
       <VStack>
-        <Text color="gray.100">
+        <Text color="gray.100" fontSize="md">
           Olá,
         </Text>
 
-        <Heading color="gray.100">
+        <Heading color="gray.100" fontSize="md">
           Rodrigo
         </Heading>
       </VStack>
