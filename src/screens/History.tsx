dd9b213b39ef9470d04f7dfb4f8b3b95c851e751
RennanD <rplaykl@gmail.com>
2022-10-04 17:12:20 -0300

    Finalizando as Interfaces - Mensagem de lista vazia

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Heading, VStack, SectionList } from 'native-base';
+import { Heading, VStack, SectionList, Text } from 'native-base';
 
 import { HistoryCard } from '@components/HistoryCard';
 import { ScreenHeader } from '@components/ScreenHeader';
@@ -32,6 +32,13 @@ export function History() {
           </Heading>
         )}
         px={8}
+        contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: 'center' }}
+        ListEmptyComponent={() => (
+          <Text color="gray.100" textAlign="center">
+            Não há exercícios registrados ainda. {'\n'}
+            Vamos fazer exercícios hoje?
+          </Text>
+        )}
       />
 
     </VStack>
