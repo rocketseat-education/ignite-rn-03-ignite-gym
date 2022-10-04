db954ebcc2ed90843fd136e8c10227d2da6c743a
RennanD <rplaykl@gmail.com>
2022-10-04 17:55:36 -0300

    Finalizando as Interfaces - Inputs de nome e e-mails

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -4,6 +4,7 @@ import { Center, ScrollView, VStack, Skeleton, Text } from 'native-base';
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
+import { Input } from '@components/Input';
 
 
 const PHOTO_SIZE = 33;
@@ -40,6 +41,17 @@ export function Profile() {
               Alterar Foto
             </Text>
           </TouchableOpacity>
+
+          <Input 
+            bg="gray.600" 
+            placeholder='Nome' 
+          />
+
+          <Input 
+            bg="gray.600" 
+            placeholder="E-mail"
+            isDisabled
+          />
         </Center>
       </ScrollView>
     </VStack>
