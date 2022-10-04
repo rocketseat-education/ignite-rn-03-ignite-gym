f6224300d450234b91a2c1983711b2cf43aaf53b
RennanD <rplaykl@gmail.com>
2022-10-04 14:52:23 -0300

    Finalizando as Interfaces - Exibindo o grupo selecionado

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -2,9 +2,10 @@ import { Text, Pressable, IPressableProps } from 'native-base';
 
 type Props = IPressableProps & {
   name: string;
+  isActive: boolean
 }
 
-export function Group({ name, ...rest }: Props) {
+export function Group({ name, isActive, ...rest }: Props) {
   return (
     <Pressable
       mr={3}
@@ -15,6 +16,7 @@ export function Group({ name, ...rest }: Props) {
       justifyContent="center"
       alignItems="center"
       overflow="hidden"
+      isPressed={isActive}
       _pressed={{
         borderColor: 'green.500',
         borderWidth: 1
@@ -22,7 +24,7 @@ export function Group({ name, ...rest }: Props) {
       {...rest}
     >
       <Text
-        color="gray.200"
+        color={isActive ? "green.500" : "gray.200"}
         textTransform="uppercase"
         fontSize="xs"
         fontWeight="bold"
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,15 +2,27 @@ import { HStack, VStack } from 'native-base';
 
 import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
+import { useState } from 'react';
 
 export function Home() {
+
+  const [groupSelected, setGroupSelected] = useState('costa')
+
   return (
     <VStack flex={1}>
       <HomeHeader />
 
       <HStack>
-        <Group name="Costa" />
-        <Group name="ombro" />
+        <Group 
+          name="costa" 
+          isActive={groupSelected === 'costa'}
+          onPress={() => setGroupSelected('costa')}
+        />
+        <Group 
+          name="ombro" 
+          isActive={groupSelected === 'ombro'}
+          onPress={() => setGroupSelected('ombro')}
+        />
       </HStack>
 
     </VStack>
