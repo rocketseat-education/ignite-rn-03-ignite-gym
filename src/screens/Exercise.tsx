570d97548e4b0de1ee7add05e13d096f8907596b
RennanD <rplaykl@gmail.com>
2022-10-04 19:05:45 -0300

    Finalizando as Interfaces - Imagem do Exercício

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,5 +1,5 @@
 import { TouchableOpacity } from 'react-native';
-import { Heading, HStack, Icon, Text, VStack } from 'native-base';
+import { Heading, HStack, Icon, Image, Text, VStack } from 'native-base';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 
@@ -40,6 +40,18 @@ export function Exercise() {
             </Text>
           </HStack>
         </HStack>
+      </VStack>
+
+      <VStack p={8}>
+        <Image
+          w="full"
+          h={80}
+          source={{ uri: 'http://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg' }}
+          alt="Nome do exercício"
+          mb={3}
+          resizeMode="cover"
+          rounded="lg"
+        />
       </VStack>
     </VStack>
   );
