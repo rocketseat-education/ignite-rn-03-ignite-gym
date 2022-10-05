a1fab0e017a4db2f83b4dad3a7be62ba83c6a1f0
RennanD <rplaykl@gmail.com>
2022-10-05 19:19:31 -0300

    Formulários - Obtendo a mensagem de erro

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -17,7 +17,7 @@ type FormDataProps = {
 
 export function SignUp() {
   
-  const { control, handleSubmit } = useForm<FormDataProps>();
+  const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>();
 
   const navigation = useNavigation();
 
@@ -67,6 +67,8 @@ export function SignUp() {
               />
             )}
           />
+
+          <Text color="white">{errors.name?.message}</Text>
 
           <Controller 
             control={control}
