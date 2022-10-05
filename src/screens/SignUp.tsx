2c745b0cd218df4b60bf688004ae973600c2a620
RennanD <rplaykl@gmail.com>
2022-10-05 18:45:58 -0300

    Formulários - Inputs Controlados

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+import { useForm, Controller } from 'react-hook-form';
 
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
@@ -9,10 +9,8 @@ import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
 export function SignUp() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [passwordConfirm, setPasswordConfirm] = useState('');
+  
+  const { control } = useForm();
 
   const navigation = useNavigation();
 
@@ -21,12 +19,7 @@ export function SignUp() {
   }
 
   function handleSignUp() {
-    console.log({
-      name,
-      email,
-      password,
-      passwordConfirm
-    });
+
   }
 
   return (
@@ -53,26 +46,56 @@ export function SignUp() {
             Crie sua conta
           </Heading>
 
-          <Input 
-            placeholder="Nome"
-            onChangeText={setName}
+          <Controller 
+            control={control}
+            name="name"
+            render={({ field: { onChange, value } }) => (
+              <Input 
+                placeholder="Nome"
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
           />
 
-          <Input 
-            placeholder="E-mail" 
-            keyboardType="email-address"
-            autoCapitalize="none"
-            onChangeText={setEmail}
+          <Controller 
+            control={control}
+            name="email"
+            render={({ field: { onChange, value } }) => (
+              <Input 
+                placeholder="E-mail" 
+                keyboardType="email-address"
+                autoCapitalize="none"
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
           />
-          <Input 
-            placeholder="Senha" 
-            secureTextEntry
-            onChangeText={setPassword}
+
+          <Controller 
+            control={control}
+            name="password"
+            render={({ field: { onChange, value } }) => (
+              <Input 
+                placeholder="Senha" 
+                secureTextEntry
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
           />
-          <Input 
-            placeholder="Confirmar a Senha" 
-            secureTextEntry
-            onChangeText={setPasswordConfirm}
+
+          <Controller 
+            control={control}
+            name="password_confirm"
+            render={({ field: { onChange, value } }) => (
+              <Input 
+                placeholder="Confirmar a Senha" 
+                secureTextEntry
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
           />
 
           <Button 
