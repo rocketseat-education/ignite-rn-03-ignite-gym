475f71e21f4cdee38126c222327d1bcef3c1b714
RennanD <rplaykl@gmail.com>
2022-10-05 19:30:08 -0300

    Formulários - Rules Patterns

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -73,6 +73,13 @@ export function SignUp() {
           <Controller 
             control={control}
             name="email"
+            rules={{
+              required: 'Informe o email.',
+              pattern: {
+                value:/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: 'E-mail inválido'
+              }
+            }}
             render={({ field: { onChange, value } }) => (
               <Input 
                 placeholder="E-mail" 
@@ -84,6 +91,8 @@ export function SignUp() {
             )}
           />
 
+          <Text color="white">{errors.email?.message}</Text>
+          
           <Controller 
             control={control}
             name="password"
