f318b1e65dc3ebfa5b17d058dba11bc340c99995
RennanD <rplaykl@gmail.com>
2022-10-05 15:51:54 -0300

    Galeria de Fotos - Exibindo a foto selecionada

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -14,6 +14,7 @@ const PHOTO_SIZE = 33;
 export function Profile() {
 
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState('https://github.com/rodrigorgtic.png');
 
   async function handleUserPhotoSelected(){
     const photoSelected = await ImagePicker.launchImageLibraryAsync({
@@ -27,7 +28,7 @@ export function Profile() {
       return;
     }
 
-    console.log(photoSelected)
+    setUserPhoto(photoSelected.uri);
   }
  
   return (
@@ -47,7 +48,7 @@ export function Profile() {
               />
             :
               <UserPhoto 
-                source={{ uri: 'https://github.com/rodrigorgtic.png' }}
+                source={{ uri: userPhoto }}
                 alt="Foto do usuário"
                 size={PHOTO_SIZE}
               />
