731c8ebd9e66951be4d61066b59c76ac27a01b82
RennanD <rplaykl@gmail.com>
2022-10-06 17:50:20 -0300

    Formulários - Validando senha

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -18,8 +18,9 @@ type FormDataProps = {
 }
 
 const signUpSchema = yup.object({
-  name: yup.string().required('Informe o nome'),
-  email: yup.string().required('Informe o e-mail').email('E-mail inválido')
+  name: yup.string().required('Informe o nome.'),
+  email: yup.string().required('Informe o e-mail').email('E-mail inválido.'),
+  password: yup.string().required('Informe a senha').min(6, 'A senha deve ter pelo menos 6 dígitos.'),
 });
 
 export function SignUp() {
@@ -99,6 +100,7 @@ export function SignUp() {
                 secureTextEntry
                 onChangeText={onChange}
                 value={value}
+                errorMessage={errors.password?.message}
               />
             )}
           />
