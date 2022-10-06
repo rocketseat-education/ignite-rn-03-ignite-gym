67b51433ca851b7a682ce6f0b4fb661381a73e6a
RennanD <rplaykl@gmail.com>
2022-10-06 15:37:57 -0300

    Formulários - Estilizando Inputs inválidos

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -18,6 +18,11 @@ export function Input({ errorMessage = null, isInvalid, ...rest }: Props) {
         color="white"
         fontFamily="body"
         placeholderTextColor="gray.300"
+        isInvalid={invalid}
+        _invalid={{
+          borderWidth: 1,
+          borderColor: "red.500"
+        }}
         _focus={{
           bgColor: 'gray.700',
           borderWidth: 1,
